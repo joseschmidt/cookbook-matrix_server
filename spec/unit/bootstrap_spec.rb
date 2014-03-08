996c050fd74d8582a8cb109721984fc4efdfd9f3
Doc Walker <doc.walker@jameshardie.com>
2014-03-07 21:31:47 -0600

    Add matrix_server::bootstrap recipe

---
+++ spec/unit/bootstrap_spec.rb
@@ -0,0 +1,37 @@
+# encoding: utf-8
+require 'spec_helper'
+
+describe 'matrix_server::bootstrap' do
+  cached(:chef_run) do
+    ChefSpec::Runner.new do |node|
+      # required for sysctl cookbook
+      node.set['platform_family'] = 'rhel'
+    end.converge(described_recipe)
+  end # cached
+
+  #----------------------------------------------------------------- package[]
+  describe 'man' do
+    it 'installs described package' do
+      expect(chef_run).to install_package(subject)
+    end # it
+  end # describe
+
+  describe 'nano' do
+    it 'installs described package' do
+      expect(chef_run).to install_package(subject)
+    end # it
+  end # describe
+
+  describe 'wget' do
+    it 'installs described package' do
+      expect(chef_run).to install_package(subject)
+    end # it
+  end # describe
+
+  describe 'which' do
+    it 'installs described package' do
+      expect(chef_run).to install_package(subject)
+    end # it
+  end # describe
+
+end # describe
--- spec/unit/default_spec.rb
+++ spec/unit/default_spec.rb
@@ -61,6 +61,12 @@ describe 'matrix_server' do
     end # it
   end # describe
 
+  describe 'matrix_server::bootstrap' do
+    it 'includes described recipe' do
+      expect(chef_run).to include_recipe(subject)
+    end # it
+  end # describe
+
   describe 'matrix_server::network' do
     it 'includes described recipe' do
       expect(chef_run).to include_recipe(subject)
