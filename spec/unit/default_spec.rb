47e4822297bad8da717a038b21861481609018bb
Doc Walker <doc.walker@jameshardie.com>
2014-03-09 08:33:49 -0500

    Add sysctl to default recipe

--- spec/unit/default_spec.rb
+++ spec/unit/default_spec.rb
@@ -127,4 +127,10 @@ describe 'matrix_server' do
     end # it
   end # describe
 
+  describe 'matrix_server::sysctl' do
+    it 'includes described recipe' do
+      expect(chef_run).to include_recipe(subject)
+    end # it
+  end # describe
+
 end # describe
