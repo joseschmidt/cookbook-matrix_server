d79227d850bef2be21f07bef6137abf37dc3e194
Doc Walker <doc.walker@jameshardie.com>
2014-03-09 20:22:54 -0500

    Update network attributes

--- attributes/network.rb
+++ attributes/network.rb
@@ -4,8 +4,16 @@
 # Attributes:: network
 #
 
-default['network']['hosts']['127.0.0.1'] =
+default['network']['hosts']['127.0.0.1']  =
   %w(localhost.localdomain localhost)
 ipaddress = node['ipaddress']
-default['network']['hosts'][ipaddress] = [node['fqdn'], node['hostname']]
-default['network']['firewall'] = [22]
+default['network']['hosts'][ipaddress]    = [node['fqdn'], node['hostname']]
+default['network']['firewall']            = [
+  22,     # ssh
+  80,     # http
+  3306,   # mysqld
+  4000,   # chef 10.x json
+  4040,   # chef 10.x http
+  5666,   # avamar client
+  28_002  # avamar client (?)
+]
