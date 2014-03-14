304da1aa2fedef8121c33470161b715816422053
Doc Walker <doc.walker@jameshardie.com>
2014-03-13 20:27:05 -0500

    Fix #18 replace simple_iptables with template

--- attributes/network.rb
+++ attributes/network.rb
@@ -12,8 +12,6 @@ default['network']['firewall']            = [
   22,     # ssh
   80,     # http
   3306,   # mysqld
-  4000,   # chef 10.x json
-  4040,   # chef 10.x http
   5666,   # avamar client
   28_002  # avamar client (?)
 ]
--- spec/unit/network_spec.rb
+++ spec/unit/network_spec.rb
@@ -100,89 +100,27 @@ describe 'matrix_server::network' do
         end # it
       end # describe
 
-      #------------------------------------------------------ include_recipe[]
-      describe 'simple_iptables' do
-        it 'includes described recipe' do
-          expect(chef_run).to include_recipe(subject)
+      #------------------------------------- template[/etc/sysconfig/iptables]
+      describe '/etc/sysconfig/iptables' do
+        it 'creates template with expected owner, group, mode' do
+          expect(chef_run).to create_template(subject)
+            .with(:owner => 'root', :group => 'root', :mode => '0600')
         end # it
-      end # describe
 
-      #---------------------------------------------- simple_iptables_policy[]
-      describe 'INPUT' do
-        it 'sets expected chain policy' do
-          expect(chef_run).to set_iptables_chain(subject)
-            .with_policy('DROP')
+        it 'renders file with expected header' do
+          expect(chef_run).to render_file(subject)
+            .with_content('-m tcp -p tcp --dport 2 --jump ACCEPT')
         end # it
-      end # describe
 
-      describe 'FORWARD' do
-        it 'sets expected chain policy' do
-          expect(chef_run).to set_iptables_chain(subject)
-            .with_policy('DROP')
+        it 'renders file with expected header' do
+          expect(chef_run).to render_file(subject)
+            .with_content('-m tcp -p tcp --dport 178 --jump ACCEPT')
         end # it
-      end # describe
 
-      describe 'OUTPUT' do
-        it 'sets expected chain policy' do
-          expect(chef_run).to set_iptables_chain(subject)
-            .with_policy('ACCEPT')
-        end # it
-      end # describe
-
-      #------------------------------------------------ simple_iptables_rule[]
-      describe 'accept_localhost' do
-        it 'appends expected rule' do
-          expect(chef_run).to append_iptables_rule(subject)
-            .with_chain('RH-Firewall-1-INPUT')
-            .with_rule('-i lo')
-            .with_jump('ACCEPT')
-        end # it
-      end # describe
-
-      describe 'drop_unless_begins_with_syn' do
-        it 'appends expected rule' do
-          expect(chef_run).to append_iptables_rule(subject)
-            .with_chain('RH-Firewall-1-INPUT')
-            .with_rule('-p tcp ! --syn -m state --state NEW')
-            .with_jump('DROP')
-        end # it
-      end # describe
-
-      describe 'accept_icmp' do
-        it 'appends expected rule' do
-          expect(chef_run).to append_iptables_rule(subject)
-            .with_chain('RH-Firewall-1-INPUT')
-            .with_rule(['-p icmp --icmp-type 8', '-p icmp --icmp-type 11'])
-            .with_jump('ACCEPT')
-        end # it
-      end # describe
-
-      describe 'accept_established_related' do
-        it 'appends expected rule' do
-          expect(chef_run).to append_iptables_rule(subject)
-            .with_chain('RH-Firewall-1-INPUT')
-            .with_rule('-m state --state ESTABLISHED,RELATED')
-            .with_jump('ACCEPT')
-        end # it
-      end # describe
-
-      [2, 178].each do |port|
-        describe "accept_port_#{port}" do
-          it 'appends expected rule' do
-            expect(chef_run).to append_iptables_rule(subject)
-              .with_chain('RH-Firewall-1-INPUT')
-              .with_rule("-m state --state NEW -m tcp -p tcp --dport #{port}")
-              .with_jump('ACCEPT')
-          end # it
-        end # describe
-      end # [...]
-
-      describe 'reject_other_inbound' do
-        it 'appends expected rule' do
-          expect(chef_run).to append_iptables_rule(subject)
-            .with_chain('RH-Firewall-1-INPUT')
-            .with_rule('')
-            .with_jump('REJECT --reject-with icmp-host-prohibited')
+        it 'notifies resource with expected action' do
+          resource = chef_run.template(subject)
+          expect(resource).to notify('service[iptables]')
+            .to(:reload).delayed
         end # it
       end # describe
 
