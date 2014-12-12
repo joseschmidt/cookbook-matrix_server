2f56687d3288cb1ffe103b48ffced7cc15d7049c
Doc Walker <doc.walker@jameshardie.com>
2014-12-11 19:45:33 -0600

    Pin sysctl cookbook to v0.6.0

--- metadata.rb
+++ metadata.rb
@@ -26,7 +26,7 @@ depends           'rvm'
 depends           'selinux', '~> 0.8.0'
 depends           'ssh_known_hosts'
 depends           'sudo', '~> 2.3.0'
-depends           'sysctl'
+depends           'sysctl', '~> 0.6.0'
 depends           'user'
 depends           'yum-epel'
 
